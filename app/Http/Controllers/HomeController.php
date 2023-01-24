27e4006f00e23aab005d608d0160b3b6feb3236f
yellaujg <yella.200180130@mhs.unimal.ac.id>
2023-01-25 02:23:52 +0700

    membuat logout function & redirect view home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('pages.index');
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerate();
+            return redirect('/login')->with(['info' => 'Thanks you, see you next time!']);
+        } catch (QueryException $th) {
+            $th->errorInfo;
+            return redirect()->back();
+        }
     }
 }
